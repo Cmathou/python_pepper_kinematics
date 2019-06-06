779b961da2185a3e6a77034554bf12f953f74495
Cmathou <47972053+Cmathou@users.noreply.github.com>
2019-06-06 10:35:19 +0200

    Add files via upload

--- pepper_kinematics/__init__.py
+++ pepper_kinematics/__init__.py
@@ -60,16 +60,21 @@ def right_arm_set_position(angles, target_pos, target_ori, epsilon=0.001):
     actual_position = right_arm_get_position(angles)[0]
 
     valid_pos = np.fabs(np.asarray(actual_position[:-1]) - np.asarray(target_pos[:-1])) >= epsilon
+    sign = np.sign(np.asarray(target_pos[:-1]) - np.asarray(actual_position[:-1]))*valid_pos
     while (valid_pos.any()):
-        if (len(ik.calc_inv_pos(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*valid_pos, 1), target_ori, epsilon, right=True)) == 0):
-            if (len(right_arm_try(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*valid_pos, 1), target_ori, epsilon)) == 0):
+        print actual_position
+        print valid_pos
+        print sign
+        if (len(ik.calc_inv_pos(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*sign, 1), target_ori, epsilon, right=True)) == 0):
+            if (len(right_arm_try(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*sign, 1), target_ori, epsilon/10.0)) == 0):
                 print '# set_position error : Distance can not converged.'
                 return np.array([])
             else:
-                actual_position = right_arm_get_position(right_arm_try(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*valid_pos, 1), target_ori, epsilon))[0]
+                actual_position = right_arm_get_position(right_arm_try(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*sign, 1), target_ori, epsilon/10.0))[0]
         else:
-            actual_position = right_arm_get_position(ik.calc_inv_pos(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*valid_pos, 1), target_ori, epsilon, right=True))[0]
+            actual_position = right_arm_get_position(ik.calc_inv_pos(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*sign, 1), target_ori, epsilon, right=True))[0]
         valid_pos = np.fabs(np.asarray(actual_position[:-1]) - np.asarray(target_pos[:-1])) >= epsilon
+        sign = np.sign(np.asarray(target_pos[:-1]) - np.asarray(actual_position[:-1]))*valid_pos
     return ik.calc_inv_pos(angles, actual_position, target_ori, epsilon, right=True)
 
 def right_arm_try(angles, target_pos, target_ori, epsilon = 0.001):
@@ -79,9 +84,11 @@ def right_arm_try(angles, target_pos, target_ori, epsilon = 0.001):
             [0.58*epsilon,-0.58*epsilon,0.58*epsilon,0],[0.58*epsilon,-0.58*epsilon,-0.58*epsilon,0],
             [-0.58*epsilon,0.58*epsilon,0.58*epsilon,0],[-0.58*epsilon,0.58*epsilon,-0.58*epsilon,0],
             [-0.58*epsilon,-0.58*epsilon,0.58*epsilon,0],[-0.58*epsilon,-0.58*epsilon,-0.58*epsilon,0]])
+    actual_position = right_arm_get_position(angles)[0]
     for i in range(1, p + 1):
         for e in val:
-            if (len(ik.calc_inv_pos(angles, np.add(target_pos, e*i/p*1.0), target_ori, epsilon, right=True)) != 0):
+            valid_pos = ((np.asarray(target_pos[:-1]) - np.asarray(actual_position[:-1])) * e[:-1] >= 0).sum()
+            if (len(ik.calc_inv_pos(angles, np.add(target_pos, e*i/p*1.0), target_ori, epsilon, right=True)) != 0 and valid_pos >= 3):
                 return ik.calc_inv_pos(angles, np.add(target_pos, e*i/p*1.0), target_ori, epsilon, right=True)
     return np.array([])        
 
@@ -101,16 +108,18 @@ def left_arm_set_position(angles, target_pos, target_ori, epsilon = 0.001):
     actual_position = left_arm_get_position(angles)[0]
 
     valid_pos = np.fabs(np.asarray(actual_position[:-1]) - np.asarray(target_pos[:-1])) >= epsilon
+    sign = np.sign(np.asarray(target_pos[:-1]) - np.asarray(actual_position[:-1]))*valid_pos
     while (valid_pos.any()):
-        if (len(ik.calc_inv_pos(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*valid_pos, 1), target_ori, epsilon, right=False)) == 0):
-            if (len(left_arm_try(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*valid_pos, 1), target_ori, epsilon)) == 0):
+        if (len(ik.calc_inv_pos(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*sign, 1), target_ori, epsilon, right=False)) == 0):
+            if (len(left_arm_try(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*sign, 1), target_ori, epsilon/10.0)) == 0):
                 print '# set_position error : Distance can not converged.'
                 return np.array([])
             else:
-                actual_position = left_arm_get_position(left_arm_try(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*valid_pos, 1), target_ori, epsilon))[0]
+                actual_position = left_arm_get_position(left_arm_try(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*sign, 1), target_ori, epsilon/10.0))[0]
         else:
-            actual_position = left_arm_get_position(ik.calc_inv_pos(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*valid_pos, 1), target_ori, epsilon, right=False))[0]
+            actual_position = left_arm_get_position(ik.calc_inv_pos(angles, np.append(np.asarray(actual_position[:-1]) + epsilon*sign, 1), target_ori, epsilon, right=False))[0]
         valid_pos = np.fabs(np.asarray(actual_position[:-1]) - np.asarray(target_pos[:-1])) >= epsilon
+        sign = np.sign(np.asarray(target_pos[:-1]) - np.asarray(actual_position[:-1]))*valid_pos
     return ik.calc_inv_pos(angles, actual_position, target_ori, epsilon, right=False)
     
 def left_arm_try(angles, target_pos, target_ori, epsilon = 0.001):
@@ -120,10 +129,12 @@ def left_arm_try(angles, target_pos, target_ori, epsilon = 0.001):
             [0.58*epsilon,-0.58*epsilon,0.58*epsilon,0],[0.58*epsilon,-0.58*epsilon,-0.58*epsilon,0],
             [-0.58*epsilon,0.58*epsilon,0.58*epsilon,0],[-0.58*epsilon,0.58*epsilon,-0.58*epsilon,0],
             [-0.58*epsilon,-0.58*epsilon,0.58*epsilon,0],[-0.58*epsilon,-0.58*epsilon,-0.58*epsilon,0]])
+    actual_position = left_arm_get_position(angles)[0]
     for i in range(1, p + 1):
         for e in val:
-            if (len(ik.calc_inv_pos(angles, np.add(target_pos, e*i/p*1.0), target_ori, epsilon, right=False)) != 0):
+            valid_pos = ((np.asarray(target_pos[:-1]) - np.asarray(actual_position[:-1])) * e[:-1] >= 0).sum()
+            if (len(ik.calc_inv_pos(angles, np.add(target_pos, e*i/p*1.0), target_ori, epsilon, right=False)) != 0 and valid_pos >= 3):
                 return ik.calc_inv_pos(angles, np.add(target_pos, e*i/p*1.0), target_ori, epsilon, right=False)
-    return np.array([])
+    return np.array([])  
 
 
